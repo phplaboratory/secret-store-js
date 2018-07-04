9ecca9eb62278144a5052eb5c38f548c7e312fda
Lenovo <php.laboratory@gmail.com>
2018-07-04 12:07:31 +0300

    Parity Secret Store encrypt and decrypt.

--- secretstore.js
+++ secretstore.js
@@ -1,58 +1,88 @@
 'use strict';
 const ecies = require('./ecies_taras.js');
 const secp256k1 = require('secp256k1');
+var rp = require('request-promise');
+var crypto = require('crypto');
 
-var request =require('request');
+
+
+const ivlen = 16;
+
 
 /*
-request key generation
-* var privateKey : string
-* var docid : string
-* var replicas : int
-* var callback(err, encryptedDocumentKey : string)
-*
-* */
+privateKey: string (hex)
+docid: string (hex)
+secretStoreUrl: string
+ */
 
-// test
-function generateKeyRequest(privateKey,docid,replicas,secretStoreUrl) {
-    return new Promise( function (resolve, reject) {
+function getKeyRequest(privateKey,docid,secretStoreUrl) {
     var privateKeyBuffer = new Buffer.from(privateKey, 'hex');
-    var docidBuf = new Buffer(docid,'hex');
+    var docidBuf = new Buffer(docid, 'hex');
     var signedStorageId = secp256k1.sign(docidBuf, privateKeyBuffer);
-    var url = secretStoreUrl+ '/'
+    var url = secretStoreUrl + '/'
+        + docid + '/'
+        + signedStorageId.signature.toString('hex') + '0' + signedStorageId.recovery.toString(16);
+
+    console.log("url:",url);
+
+    var options = {
+        uri: url,
+        qs: {
+        },
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true
+    };
+    return new Promise(function (resolve, reject) {
+        rp(options)
+            .then(function (parsedBody) {
+                resolve(parsedBody.replace('0x', ''));
+            })
+            .catch(function(error) {
+                console.log("error:",error);
+            });
+    });
+}
+
+
+
+
+function generateKeyRequest(privateKey,docid,replicas,secretStoreUrl) {
+
+    var privateKeyBuffer = new Buffer.from(privateKey, 'hex');
+    var docidBuf = new Buffer(docid, 'hex');
+    var signedStorageId = secp256k1.sign(docidBuf, privateKeyBuffer);
+    var url = secretStoreUrl + '/'
         + docid + '/'
         + signedStorageId.signature.toString('hex') + '0' + signedStorageId.recovery.toString(16) + '/' +
         replicas;
-     request.post(url, function (error, response, body) {
-        if (error) { return cb(error,null); };
-        if (!response) { return cb(new Error('No response'),null); }
-        if (response.statusCode != 200) { return cb(new Error( "HTTP: "+response.statusCode + " - " +response.statusMessage )); }
-        return cb(null,JSON.parse(body).replace('0x', ''))
-    });
 
-    })
+    var options = {
+        method: 'POST',
+        uri: url,
+        body: {},
+        json: true
+    };
+
+    return new Promise(function (resolve, reject) {
+        rp(options)
+            .then(function (parsedBody) {
+                resolve(parsedBody.replace('0x', ''));
+            })
+            .catch(reject);
+    });
 }
 
-generateKeyRequest(
-    'd2949e0ad0c1a76d20cdb7d9df77de43a2143035851f04661ce4c2254a074d07',
-    'f5c0260c9a5ed7e007819737e7788d8db33b595374df4caf411e9d457d9c8cd3',
-    0,
-    "http://94.130.94.162:8083",
-    function(err,done) {
-        if (err) {
-            console.error(err);
-        } else {
+/*
+privateKey: string (hex)
+encryptedKey: string (hex)
+ */
 
-            console.log("done:",done);
-        }
-    }
-);
-
-
-exports.decrypt_SecretStore= function(privateKey,data) {
-    let encrypted_document_key_buffer = new Buffer(data, 'hex');
+function decryptDocumentKey(privateKey,encrytedKey) {
+    let encrypted_document_key_buffer = new Buffer(encrytedKey, 'hex');
     let clen = encrypted_document_key_buffer.length - 32;
-    return exports.decrypt(privateKey, {
+    return ecies.decrypt(privateKey, {
         ephemPublicKey: encrypted_document_key_buffer.slice(0, 65),
         iv: encrypted_document_key_buffer.slice(65, 81),
         ciphertext: encrypted_document_key_buffer.slice(81, clen),
@@ -60,37 +90,59 @@ exports.decrypt_SecretStore= function(privateKey,data) {
     })
 }
 
-/*
-privateKey: string (hex)
-encryptedKey: string (hex)
 
-cb : function(error,done)
+/*
+documentBigKey : string (hex)
+document: string (hex)
+
+return: Buffer
  */
 
-function decryptDocumentKey(privateKey,encrytedKey, cb) {
-    // exports.decrypt_SecretStore= function(privateKey,data) {
-        //     let encrypted_document_key_buffer = new Buffer(encrytedKey, 'hex');
-        //     let clen = encrypted_document_key_buffer.length - 32;
-        //     return exports.decrypt(privateKey, {
-        //         ephemPublicKey: encrypted_document_key_buffer.slice(0, 65),
-        //         iv: encrypted_document_key_buffer.slice(65, 81),
-        //         ciphertext: encrypted_document_key_buffer.slice(81, clen),
-        //         mac: encrypted_document_key_buffer.slice(clen)
-        //     })
-    // }
+function decryptDocument(documentBigKey,document) {
 
-
-
-    // return documentKey;
+    var smallKey = documentBigKey.slice(0,16);
+    var source = new Buffer(document,'hex');
+    var encrypted_document = source.slice(0,source.length-ivlen);
+    var iv = source.slice(source.length-ivlen,source.length);
+    var cipher = crypto.createDecipheriv("AES-128-CTR", smallKey, iv);
+    var firstChunk = cipher.update(encrypted_document);
+    var secondChunk = cipher.final();
+    return Buffer.concat([firstChunk, secondChunk]);
 }
 
-function decryptDocument(documentKey,document) {
+/*
+low: int
+high: int
 
-    return decryptedDocument;
+
+ */
+function randomIntInc (low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
 }
 
-function encryptDocument(documentKey,document) {
 
 
-    return encryptedDocument;
+
+/*
+documentBigKey : string (hex)
+document: Buffer
+
+return: string (hex)
+ */
+
+function encryptDocument(documentBigKey,document) {
+
+    var smallKey = documentBigKey.slice(0,16);
+    var numbers = new Array(ivlen);
+    for (var i = 0; i < numbers.length; i++) {
+        numbers[i] = randomIntInc(0,255);
+    }
+    var iv  = new Buffer(numbers);
+    console.log("iv:",iv);
+    var cipher = crypto.createCipheriv("AES-128-CTR", smallKey, iv);
+    var firstChunk = cipher.update(new Buffer(document));
+    var secondChunk = cipher.final();
+    return Buffer.concat([firstChunk, secondChunk,iv]);
 }
+
+
